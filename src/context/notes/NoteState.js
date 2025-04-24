033f11218d6dbdf323d213636cbf0762846e24f8
Tanishq2645 <tanishq.bansal@techplustalent.com>
2025-04-24 20:27:25 +0530

    Update NoteState.js

--- src/context/notes/NoteState.js
+++ src/context/notes/NoteState.js
@@ -47,7 +47,8 @@ const NoteState = (props) => {
         "auth-token": "eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VyIjp7ImlkIjoiNjEzMWRjNWUzZTQwMzdjZDQ3MzRhMDY2In0sImlhdCI6MTYzMDY2OTU5Nn0.hJS0hx6I7ROugkqjL2CjrJuefA3pJi-IU5yGUbRHI4Q"
       }
     });
-    const json = response.json(); 
+    const json = response.json();
+    console.log(json);
     const newNotes = notes.filter((note) => { return note._id !== id })
     setNotes(newNotes)
   }
@@ -64,6 +65,7 @@ const NoteState = (props) => {
       body: JSON.stringify({title, description, tag})
     });
     const json = await response.json(); 
+    console.log(json);
 
      let newNotes = JSON.parse(JSON.stringify(notes))
     // Logic to edit in client
